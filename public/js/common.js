e492ef2246c8a39d70a09c895fe4926df81628d7
youzhen <yl301114@gmail.com>
2025-10-30 12:04:48 +0000

    img

--- public/js/common.js
+++ public/js/common.js
@@ -4,19 +4,6 @@ $(".header_menu").click(function() {
     $(".header").toggleClass("active");
 })
 
-// nav();
-// function nav() {
-//     $(".header_nav li").each(function() {
-//         if($(this).find("a").hasClass("active")) {
-//             let index = $(this).index();
-//             if(index => 1) {
-//                 let preIndex = index - 1;
-//                 $(".header_nav li").eq(preIndex).find("a").addClass("noline");
-//             }
-//         }
-//     })
-// }
-
 $(".btn-back").click(function() {
     history.back();
 })
